419ac472b81ea4ca06ea5ce92b49170a97c027ac
codethrx <tatheermehdi2000@gmail.com>
2023-04-02 00:11:16 +0500

    Down

---
+++ prisma/migrations/20230401184914_new_one_for/migration.sql
@@ -0,0 +1,56 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Basic', 'Admin');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'Basic',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "favId" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToPost" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_favId_fkey" FOREIGN KEY ("favId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_B_fkey" FOREIGN KEY ("B") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- server.js
+++ server.js
@@ -3,14 +3,27 @@ import { PrismaClient } from "@prisma/client";
 const app = express();
 
 const client = new PrismaClient();
+
+//reading..
 app.get("/api/users", async (req, res) => {
-  const users = await client.user.findMany();
-  res.status(200).json(users);
+  const users = await client.user.findMany({
+    where: {
+      //   age: { equals: "2" },
+      //   age: { not: "2" },
+      //   age: { in: ["2", "4"] },
+      //   email: { contains: "c" },
+      //   age: { lt: 2 },
+      AND: [{ email: { contains: "c" }, role: { equals: "Basic" } }],
+      //   writtenPosts:{contains:''}
+    },
+    // distinct: ["age"],
+    // ordeerBy: { age: "asc" },
+  });
+  res.json(users);
 });
-app.post("/api/users", async (req, res) => {
-  const user = await client.user.create({ data: { email: "abc@gmail.com" } });
-  res.status(200).json(user);
-});
+//creating..
+//updating..
+//deleting..
 app.listen(4000, () => {
   console.log(`Server listening on port ${4000}`);
 });
